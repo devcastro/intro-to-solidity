28d34b95dc25674e30d855a0d9578fe32be53b5c
David Castro <63515575+devcastro@users.noreply.github.com>
2022-01-03 12:22:00 -0500

    Create Structs.sol

---
+++ Structs.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Structs {
+    
+    struct User {
+        uint8 age;
+        string name;
+    }
+    
+    
+    mapping(address => User) public users;
+    
+    function register(uint8 age, string memory name) external returns(bool){
+        users[msg.sender] = User(age, name);
+        return true;
+    }
+}
