c624b7d97d05787a8cd57e2bfdff1013cfb98b2b
David Castro <63515575+devcastro@users.noreply.github.com>
2022-01-03 12:20:06 -0500

    Create Greeting.sol

---
+++ Greeting.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Greeting {
+    function sayHello() external pure returns (string memory) {
+        return "hello world";   
+    
+    }
+    
+}
